a8b6debf42f90b632c79f6c5d1d7394aeadd75ba
Paul Borawski <paulborawski14@gmail.com>
2019-01-05 15:21:50 -0800

    Wordsearch

---
+++ wordsearch.js
@@ -0,0 +1,28 @@
+// Wordsearch
+
+/*
+Create a function wordSearch(word,text) that searches to see whether a word word is present in the given text variable.
+
+Note it has to be a full word which means it is surround by a word boundary (spaces, end/start of string, various punctuation, ... ).
+
+Example:
+Text - "what makes the desert beautiful, said the little prince is that somewhere it hides a well";
+
+wordSearch("prince",text)--> true
+wordSearch("beautiful",text)--> true
+wordSearch("clown",text)--> false
+wordSearch("yellow",text)--> false
+wordSearch("akes the",text)--> false
+*/
+
+const myText = "what makes the desert beautiful, said the little prince is that somewhere it hides a well";
+
+function wordSearch(word, text){
+  let substring = word;
+  if text.includes(' ') {
+    return false;
+  }
+  return text.includes(substring);
+}
+
+wordSearch("desert", myText)
