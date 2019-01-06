e253fe1963b028e903349486d178d1d27392f7d3
Dylan Ellison <dylancellison@gmail.com>
2019-01-05 20:21:04 -0600

    change map to forEach

--- movingZerosToTheEnd.js
+++ movingZerosToTheEnd.js
@@ -4,10 +4,10 @@
 // Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 // moveZeros([false,1,0,1,2,0,1,3,"a"])  returns[false,1,1,2,1,3,"a",0,0]
 
-var moveZeros = function (arr) {
-  var other = [];
-  var zeroes = [];
- arr.map(item => {
+const moveZeros = function (arr) {
+  let other = [], zeroes = [];
+
+  arr.forEach(item => {
    if (item === 0 || item === '0') {
      zeroes.push(item);
    }
